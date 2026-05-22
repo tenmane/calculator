d87e35dfbb5908c7aa286fd1b7f032479dfacca1
tenmane <235837458+tenmane@users.noreply.github.com>
2026-05-22 21:33:29 +0400

    Add basic functionality to calculator
    Respond to user input and display results of calculations
    Add pair adding to calculator
    Round output to 2 decimals

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const buttons = document.querySelectorAll(".button");
+const output = document.querySelector(".output");
+let a = "";
+let b = "";
+let op = "";
+let rslt = "";
+let temp = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -28,3 +36,52 @@ function operator(op, a, b) {
     return divide(a, b);
   }
 }
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", (e) => {
+    if (e.target.parentElement.classList.contains("operators")) {
+      if (a != "" && b != "") {
+        temp = operator(op, Number(a), Number(b));
+        output.textContent = temp.toFixed(2);
+        a = temp;
+        b = "";
+      }
+      op = e.target.textContent;
+    }
+    if (e.target.parentElement.classList.contains("digits") && e.target.textContent != "=") {
+      if (op == "") {
+        a += e.target.textContent;
+        output.textContent = a;
+      }
+      else {
+        if (e.target.textContent != "=") {
+          b += e.target.textContent;
+          output.textContent = b;
+        }
+      }
+    }
+    if (e.target.textContent == "=") {
+      if (b == "") {
+        console.log(a);
+        rslt = a;
+        output.textContent = rslt;
+        return;
+      }
+      console.log(op);
+      console.log(a);
+      console.log(b);
+      rslt = operator(op, Number(a), Number(b));
+      output.textContent = rslt.toFixed(2);
+      a = rslt;
+      b = "";
+      op = "";
+      rslt = "";
+    }
+    if (e.target.textContent == "C") {
+      output.textContent = "";
+      a = "";
+      b = "";
+      op = "";
+    }
+  })
+});
